d191446594a55e1e5d504288b6ba3c941248b8a3
arthurgalimberti <137822105+arthurgalimberti@users.noreply.github.com>
2025-02-14 02:18:29 -0300

    Add files via upload

---
+++ submission/fatorial.asm
@@ -0,0 +1,24 @@
+	SC FAT	; Chamada da subrotina
+	HM = 0	; Fim do programa
+@ /100
+N 	K = 0	; var N
+RES 	K = 1	; var RES
+CTE1 	K = 1	; const 1
+@ /200
+FAT	K = 0	; Inicio da subrotina
+	LD N	; AC = N
+ 	JZ FIM	; Se N == 0, jump pro fim da rotina
+ 	
+LOOP	LD RES	; AC = RES
+	ML N	; AC = RES * N
+	MM RES	; RES = RES * N
+	
+	LD N	; AC = N
+	SB CTE1	; AC = N - 1
+	MM N	; N = N - 1
+	
+	JZ FIM	; Se N == 0, jump pro fim da rotina
+	JP LOOP	; Loop enquanto N != 0
+	
+FIM 	RS FAT	; Fim da subrotina
+
